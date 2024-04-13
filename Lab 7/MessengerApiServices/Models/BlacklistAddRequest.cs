6ca51aa7d60fccbde7a5b6a7518fc9bae43818a8
Nick <mrdoggydog37@gmail.com>
2024-04-13 08:58:37 +0200

    Finished pr 5

--- Lab 7/MessengerApiServices/Models/BlacklistAddRequest.cs
+++ Lab 7/MessengerApiServices/Models/BlacklistAddRequest.cs
@@ -2,12 +2,14 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 
 namespace MessengerModels.Models
 {
     public class BlacklistAddRequest
     {
+        [JsonPropertyName("userId")]
         public Guid UserId { get; set; }
     }
 }
